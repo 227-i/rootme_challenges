6f8d082cb68cee79f11d4c5e2922d989c091915e
Isaac <4953414143b@gmail.com>
2026-02-07 01:02:09 +0100

    succès challenge tcp - retour au collège

--- programmation/tcp_-_retour_au_college/main.c
+++ programmation/tcp_-_retour_au_college/main.c
@@ -5,14 +5,54 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define TAILLE_BUFF 80
+#include <math.h>
+#define TAILLE_BUFF 256
 
 
 void erreur(const char* msg)
 {
-    perror(msg);
-    exit(EXIT_FAILURE);
+    printf("Erreur: %s\n",msg);
+    exit(1);
 }
+
+
+void traitement_nombres(char* buffer, int* nb1, int* nb2)
+{
+    int nombre1[10];
+    int nombre2[10];
+    memset(&nombre1,-1,sizeof(nombre1));
+    memset(&nombre2,-1,sizeof(nombre2));
+
+    // positionne l'indice i sur le premier nombre, dépend du message envoyé par le serveur mais inscrit en dur ici
+    int i = 171;
+    // récupère le premier nombre
+    int j = 0;
+    while(i<strlen(buffer) && j < 10 && buffer[i] != ' ')
+    {
+        nombre1[j] = buffer[i];
+        j++;
+        i++;
+    }
+
+    i += 17;
+    // récupère le second nombre
+    int k = 0;
+    while(i<strlen(buffer) && k < 10 && buffer[i] != ' ')
+    {
+        nombre2[k] = buffer[i];
+        k++;
+        i++;
+    }
+
+    if(j <= 0 || j >= 10 || k <= 0 || k >= 10) erreur("traitement_nombres");
+
+    // j et k indiquent les tailles des tableaux
+    for(int i=0;i<j;i++)
+        *nb1 += (nombre1[i]-'0')*pow(10,j-i-1);
+    for(int i=0;i<k;i++)
+        *nb2 += (nombre2[i]-'0')*pow(10,k-i-1);
+}
+
 
 int main()
 {
@@ -51,8 +91,41 @@ int main()
     // struct sockaddr: type général des adresses
     if(connect(socketC,(struct sockaddr*) &servaddr,sizeof(servaddr)) != 0) erreur("connect");
 
-    // TODO: traitement
-    printf("Done\n");
+    // lecture de deux nombres fournis par le serveur
+    char buff[TAILLE_BUFF];
+    memset(&buff,0,sizeof(buff));
+    read(socketC,buff,sizeof(buff));
+    /* message de la forme:
+    ====================
+    GO BACK TO COLLEGE
+    ====================
+    You should tell me the answer of this math operation in less than 2 seconds !
+
+    Calculate the square root of 799 and multiply by 6223 =
+    */
+    printf("Message:\n%s\n\n",buff);
+    int nb1 = 0; int nb2 = 0;
+    traitement_nombres(buff,&nb1,&nb2);
+    printf("Nombre 1: %i\tnombre 2: %i\n",nb1,nb2);
+
+
+    // calcul du résultat à envoyer
+    double resultat = sqrt(nb1)*nb2;
+    resultat = round(resultat*100)/100.0;
+    // récupération du résultat en string
+    char buff_resultat[32];
+    memset(&buff_resultat,0,sizeof(buff_resultat));
+    snprintf(buff_resultat,sizeof(buff_resultat),"%.2f",resultat);
+    strcat(buff_resultat,"\n");
+    printf("Résultat: %s\n",buff_resultat);
+    write(socketC,buff_resultat,strlen(buff_resultat));
+
+
+    // lecture du code secret en réponse
+    char secret[TAILLE_BUFF];
+    memset(&secret,0,sizeof(secret));
+    read(socketC,secret,sizeof(secret));
+    printf("Code secret: %s\n",secret);
 
     close(socketC);
 }
